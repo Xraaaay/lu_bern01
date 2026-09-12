1bcf02806d852b3c70310dfeeb3d95c800010780
Xraaaay <xiaorw1227@gmail.com>
2026-09-12 21:53:52 +0200

    fix: adjust settings

--- Project01/code/main_density.py
+++ Project01/code/main_density.py
@@ -15,7 +15,8 @@ rho_stars = [0.1, 0.15, 0.227, 0.291, 0.38]
 T_star = 0.1
 N = 100
 
-n_runs = 10**7
+n_equi_runs = 2 * 10**6
+n_prod_runs = 2 * 10**7
 step = 1000
 
 # Run mcmc
@@ -28,20 +29,21 @@ for rho_star in rho_stars:
     model = Model(T_star, L, N)
     delta = 0.3  # TODO: acceptance ratio 0.3
 
-    U = mcmc.run_mcmc(n_runs, step, model, delta)
+    U_equi = mcmc.run_mcmc(n_equi_runs, step, model, delta, is_equi=True)
+    U_prod = mcmc.run_mcmc(n_prod_runs, step, model, delta)
 
     end_time = time.time()
     print(f"time: {end_time - start_time}s")
 
-    # TODO: calculate g(r)
-
     dir_path = f"../output/density/rho_{rho_star}/"
-    ioutil.save_ndarray(dir_path + "energy.npy", U)
-    ioutil.save_ndarray(dir_path + "config.npy", model.r_N)
+    ioutil.save_ndarray(dir_path + "energy_equi.npy", U_equi)
+    ioutil.save_ndarray(dir_path + "energy_prod.npy", U_prod)
+    ioutil.save_ndarray(dir_path + "final_config.npy", model.r_N)
     ioutil.save_json(dir_path + "metadata.json", 
                      rho_star=rho_star, 
                      T_star=T_star, 
                      N=N, 
-                     n_runs=n_runs, 
+                     n_equilibrium_runs=n_equi_runs,
+                     n_production_runs=n_prod_runs,
                      step=step, 
                      delta=delta)
