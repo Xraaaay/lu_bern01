99109b566d615d8c117eb94930e07709e37dbfe3
Xraaaay <xiaorw1227@gmail.com>
2026-09-12 21:53:52 +0200

    fix: adjust settings

--- Project01/code/main_temp.py
+++ Project01/code/main_temp.py
@@ -18,8 +18,8 @@ N = 100
 L = math.sqrt(N / rho_star)
 r_N = np.array([])
 
-n_equilibrium_runs = 10**6
-n_production_runs = 10**7
+n_equi_runs = 2 * 10**6
+n_prod_runs = 2 * 10**7
 step = 1000
 
 # Run mcmc
@@ -31,23 +31,23 @@ for T_star in T_stars:
     model = Model(T_star, L, N, r_N)
     delta = 0.3  # TODO: acceptance ratio 0.3
 
-    U_equi = mcmc.run_mcmc(n_equilibrium_runs, step, model, delta)
-    U_prod = mcmc.run_mcmc(n_production_runs, step, model, delta)
+    U_equi = mcmc.run_mcmc(n_equi_runs, step, model, delta, is_equi=True)
+    U_prod = mcmc.run_mcmc(n_prod_runs, step, model, delta)
 
     r_N = model.r_N
 
     end_time = time.time()
     print(f"time: {end_time - start_time}s")
 
-    dir_path = f"../output/temperature/T_{T_star}"
-    ioutil.save_ndarray(dir_path + "energy_equi.py", U_equi)
-    ioutil.save_ndarray(dir_path + "energy_prod.py", U_prod)
-    ioutil.save_ndarray(dir_path + "final_config.py", model.r_N)
+    dir_path = f"../output/temperature/T_{T_star}/"
+    ioutil.save_ndarray(dir_path + "energy_equi.npy", U_equi)
+    ioutil.save_ndarray(dir_path + "energy_prod.npy", U_prod)
+    ioutil.save_ndarray(dir_path + "final_config.npy", model.r_N)
     ioutil.save_json(dir_path + "metadata.json", 
                      rho_star=rho_star, 
                      T_star=T_star, 
                      N=N, 
-                     n_equilibrium_runs=n_equilibrium_runs,
-                     n_production_runs=n_production_runs,
+                     n_equilibrium_runs=n_equi_runs,
+                     n_production_runs=n_prod_runs,
                      step=step, 
                      delta=delta)
